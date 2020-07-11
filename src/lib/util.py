b35b3f9e468c42ac5b522c14e1d6915539cdd629
Daniel Dominguete <daniel.dominguete@gmail.com>
2020-07-10 21:47:50 -0300

    wip

---
+++ src/lib/util.py
@@ -0,0 +1,37 @@
+'''
+===========================================================================================
+Utils Package
+===========================================================================================
+Script Reviewed by COGNAS
+===========================================================================================
+'''
+import logging, sys
+import operator
+from functools import reduce
+import json
+import pandas as pd
+
+class Util:
+
+    def __init__(self):
+        '''Constructor for this class'''
+
+    @staticmethod
+    def load_parameters_from_file(path_file):
+
+        try:
+            with open(path_file) as json_file:
+                data = json.load(json_file)
+        except:
+            logging.error('Ops ' + str(sys.exc_info()[0]) + ' occured!')
+            raise
+
+        return data
+
+    @staticmethod
+    def save_pandas_file(dataframe:pd, file_path:str):
+        dataframe.to_csv(file_path,
+                         sep='\t',
+                         index=True,
+                         index_labe="id",
+                         encoding='uft-8')
